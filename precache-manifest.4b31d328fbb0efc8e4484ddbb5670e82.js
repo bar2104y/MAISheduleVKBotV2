50d375943618c242312644750d4b366189d42b2c
Yury Barichev <bar2104y@yandex.ru>
2020-06-18 15:33:59 +0300

    Updates

---
+++ precache-manifest.4b31d328fbb0efc8e4484ddbb5670e82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cfeb4314ec568778887adc169fe062f",
+    "url": "/TD-React-Challenge-Test-1/index.html"
+  },
+  {
+    "revision": "55c050f1bfd131f93219",
+    "url": "/TD-React-Challenge-Test-1/static/css/2.6bab1db4.chunk.css"
+  },
+  {
+    "revision": "55c050f1bfd131f93219",
+    "url": "/TD-React-Challenge-Test-1/static/js/2.af9ea668.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TD-React-Challenge-Test-1/static/js/2.af9ea668.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00484db4377e6fa0bcf2",
+    "url": "/TD-React-Challenge-Test-1/static/js/main.448037d8.chunk.js"
+  },
+  {
+    "revision": "c3b54ec93969beec60eb",
+    "url": "/TD-React-Challenge-Test-1/static/js/runtime-main.6e66ddff.js"
+  }
+]);
